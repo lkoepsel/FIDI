e4427d5cc8d1f5c34d0aa7ad8f868e51ea72c620
Lief Koepsel <lkoepsel@wellys.com>
2022-04-12 13:15:38 -0700

    updated fsm to blink led to show state

--- code.py
+++ code.py
@@ -15,7 +15,7 @@ state_1 = states(1, 2, 1, 'g', 'rb', audible_off, audible_1)
 import board
 import pwmio
 from analogio import AnalogIn
-from RGB_led import rgb
+from RGB_bin import bin_leds, blink_bin
 from proto_buttons import buttons
 from collections import namedtuple
 import supervisor
@@ -28,6 +28,8 @@ change = False
 speaker = pwmio.PWMOut(board.D3, frequency=500, duty_cycle=0,
                        variable_frequency=True)
 analog_in = AnalogIn(board.A2)
+MAGENTA = 99
+YELLOW = 98
 
 
 def null_stub():
@@ -64,29 +66,28 @@ def dutycycle():
     return(analog_in.value)
 
 
-states = namedtuple('states', ['state', 'on_D0', 'on_D1', 'led_D0', 'led_D1',
-                               'func_D0', 'func_D1'])
-state_0 = states(0, 1, 0, 'g', 'b', audible_off, null_stub)
-state_1 = states(1, 2, 1, 'g', 'rb', audible_off, audible_1)
-state_2 = states(2, 3, 2, 'g', 'gb', audible_off, ultra_1)
-state_3 = states(3, 0, 3, 'g', 'rgb', audible_off, ultra_2)
+states = namedtuple('states', ['state', 'on_D0', 'on_D1', 'func_D0', 'func_D1'])
+state_0 = states(0, 1, 0, audible_off, null_stub)
+state_1 = states(1, 2, 1, audible_off, audible_1)
+state_2 = states(2, 3, 2, audible_off, ultra_1)
+state_3 = states(3, 0, 3, audible_off, ultra_2)
 
 
 def states(STATE):
     if pressed == "D0":
         state = STATE.on_D0
-        rgb(STATE.led_D0)
+        bin_leds(STATE.state)
         STATE.func_D0()
         print(f"state {STATE.state} UP {state=}")
 
     elif pressed == "D1":
         state = STATE.on_D1
-        rgb(STATE.led_D1)
+        blink_bin(MAGENTA, STATE.state)
         STATE.func_D1()
         print(f"state {STATE.state} ENTER {state=}")
 
     else:
-        rgb('r')
+        bin_leds(5)
 
     return(state)
 
---
+++ lib/RGB_bin.py
@@ -0,0 +1,54 @@
+import time
+import board
+import pwmio
+
+
+ON = 0
+DIM = 63000
+MED = 32768
+OFF = 65535
+MAGENTA = 99
+YELLOW = 98
+
+# LED setup for most CircuitPython boards:
+RED = pwmio.PWMOut(board.LED_R, frequency=1000, duty_cycle=OFF)
+GREEN = pwmio.PWMOut(board.LED_G, frequency=1000, duty_cycle=OFF)
+BLUE = pwmio.PWMOut(board.LED_B, frequency=1000, duty_cycle=OFF)
+
+
+def bin_leds(value):
+    if value == 0:
+        RED.duty_cycle = OFF
+        GREEN.duty_cycle = DIM
+        BLUE.duty_cycle = OFF
+    elif value == 1:
+        RED.duty_cycle = OFF
+        GREEN.duty_cycle = OFF
+        BLUE.duty_cycle = ON
+    elif value == 2:
+        RED.duty_cycle = OFF
+        GREEN.duty_cycle = MED
+        BLUE.duty_cycle = OFF
+    elif value == 3:
+        RED.duty_cycle = OFF
+        GREEN.duty_cycle = MED
+        BLUE.duty_cycle = ON
+    elif value == MAGENTA:
+        RED.duty_cycle = MED
+        GREEN.duty_cycle = OFF
+        BLUE.duty_cycle = MED
+    elif value == YELLOW:
+        RED.duty_cycle = MED
+        GREEN.duty_cycle = MED
+        BLUE.duty_cycle = OFF
+    else:
+        RED.duty_cycle = ON
+        GREEN.duty_cycle = OFF
+        BLUE.duty_cycle = OFF
+
+def blink_bin(A, B):
+    for i in range(2):
+        bin_leds(A)
+        time.sleep(0.1)
+        bin_leds(B)
+        time.sleep(0.1)
